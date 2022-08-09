5f5be57e95876e64d83bbc809375191becc20a09
yusei0211 <saito31126@icloud.com>
2022-08-09 17:52:41 +0900

    08-5完成！

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,4 +28,15 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
 }
